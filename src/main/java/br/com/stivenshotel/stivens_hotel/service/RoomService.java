ea1fffa8d2ae06c2854a97ca41f33e1ad0140378
StivenF0 <stivenfonseca201@gmail.com>
2025-10-10 21:37:34 -0300

    fix: change broken logic of updating and creating new rooms

--- src/main/java/br/com/stivenshotel/stivens_hotel/service/RoomService.java
+++ src/main/java/br/com/stivenshotel/stivens_hotel/service/RoomService.java
@@ -2,8 +2,11 @@ package br.com.stivenshotel.stivens_hotel.service;
 
 import br.com.stivenshotel.stivens_hotel.enums.RoomStatus;
 import br.com.stivenshotel.stivens_hotel.model.Room;
+import br.com.stivenshotel.stivens_hotel.model.RoomType;
 import br.com.stivenshotel.stivens_hotel.repository.RoomRepository;
+import br.com.stivenshotel.stivens_hotel.repository.RoomTypeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,18 +14,29 @@ import java.util.Optional;
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
+    private final RoomTypeRepository roomTypeRepository;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, RoomTypeRepository roomTypeRepository) {
         this.roomRepository = roomRepository;
+        this.roomTypeRepository = roomTypeRepository;
     }
 
+
+    @Transactional
     public Room create(Room room) {
         if (roomRepository.findByNumber(room.getNumber()).isPresent()) {
             throw new RuntimeException("Room with number " + room.getNumber() + " already exists.");
         }
 
+        // Confere se o tipo de quarto existe
+        RoomType roomType = roomTypeRepository.findById(room.getRoomType().getId())
+                .orElseThrow(() -> new RuntimeException("RoomType not found"));
+
         // Define o status inicial para qualquer novo quarto
         room.setStatus(RoomStatus.AVAILABLE);
+
+        // Define o tipo de quarto completo
+        room.setRoomType(roomType);
 
         return roomRepository.save(room);
     }
@@ -36,19 +50,20 @@ public class RoomService {
                 .orElseThrow(() -> new RuntimeException("Room not found by id: " + id));
     }
 
+    @Transactional
     public Room update(Long id, Room roomDetails) {
-        Optional<Room> optionalRoom = roomRepository.findById(id);
-        if (!optionalRoom.isPresent()) {
-            throw new RuntimeException("Room not found by id: " + id);
-        }
+        Room existingRoom = findById(id);
 
-        Room room = optionalRoom.get();
-        room.setId(roomDetails.getId());
-        room.setFloor(roomDetails.getFloor());
-        room.setStatus(roomDetails.getStatus());
-        room.setRoomType(roomDetails.getRoomType());
+        // Confere se o tipo de quarto existe
+        RoomType roomType = roomTypeRepository.findById(roomDetails.getRoomType().getId())
+                .orElseThrow(() -> new RuntimeException("RoomType not found"));
 
-        return roomRepository.save(room);
+        existingRoom.setNumber(roomDetails.getNumber());
+        existingRoom.setFloor(roomDetails.getFloor());
+        existingRoom.setStatus(roomDetails.getStatus());
+        existingRoom.setRoomType(roomType);
+
+        return roomRepository.save(existingRoom);
     }
 
     public void delete(Long id) {
