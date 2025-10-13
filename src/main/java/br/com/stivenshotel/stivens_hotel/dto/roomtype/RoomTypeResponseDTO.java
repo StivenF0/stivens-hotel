4e62ab3c20a651efb9e122b70883c45571cd64c6
StivenF0 <stivenfonseca201@gmail.com>
2025-10-13 10:53:35 -0300

    fix: RoomTypeResponseDTO syntax error

--- src/main/java/br/com/stivenshotel/stivens_hotel/dto/roomtype/RoomTypeResponseDTO.java
+++ src/main/java/br/com/stivenshotel/stivens_hotel/dto/roomtype/RoomTypeResponseDTO.java
@@ -3,8 +3,9 @@ package br.com.stivenshotel.stivens_hotel.dto.roomtype;
 import java.math.BigDecimal;
 
 public record RoomTypeResponseDTO(
-    Long id,
-    String name,
-    String description,
-    BigDecimal dailyPrice,
-) {}
+        Long id,
+        String name,
+        String description,
+        BigDecimal dailyPrice
+) {
+}
