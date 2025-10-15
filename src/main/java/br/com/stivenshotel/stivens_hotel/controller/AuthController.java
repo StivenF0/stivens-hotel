a86274fa287e4c4353fd5071feee33b66bd98bec
StivenF0 <stivenfonseca201@gmail.com>
2025-10-14 22:24:28 -0300

    feat: implement DTO's for Authentication

--- src/main/java/br/com/stivenshotel/stivens_hotel/controller/AuthController.java
+++ src/main/java/br/com/stivenshotel/stivens_hotel/controller/AuthController.java
@@ -1,7 +1,9 @@
 package br.com.stivenshotel.stivens_hotel.controller;
 
-import br.com.stivenshotel.stivens_hotel.model.User;
+import br.com.stivenshotel.stivens_hotel.dto.auth.LoginRequestDTO;
+import br.com.stivenshotel.stivens_hotel.dto.auth.TokenResponseDTO;
 import br.com.stivenshotel.stivens_hotel.service.TokenService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,13 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
 
@@ -27,18 +25,15 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody User loginData) {
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequest) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(
-                loginData.getUsername(),
-                loginData.getPassword()
+                loginRequest.name(),
+                loginRequest.password()
         );
 
         Authentication auth = authenticationManager.authenticate(usernamePassword);
-        String token = tokenService.generateToken(auth);
+        TokenResponseDTO tokenResponse = tokenService.generateToken(auth);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("token", token);
-
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(tokenResponse);
     }
 }
--- src/main/java/br/com/stivenshotel/stivens_hotel/service/TokenService.java
+++ src/main/java/br/com/stivenshotel/stivens_hotel/service/TokenService.java
@@ -1,11 +1,14 @@
 package br.com.stivenshotel.stivens_hotel.service;
 
+import br.com.stivenshotel.stivens_hotel.dto.auth.TokenResponseDTO;
 import br.com.stivenshotel.stivens_hotel.model.User;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -21,33 +24,46 @@ public class TokenService {
     @Value("${jwt.expiration}")
     private Long expiration;
 
-    public String generateToken(Authentication authentication) {
+    public TokenResponseDTO generateToken(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         Date now = new Date();
         Date expirationDate = new Date(now.getTime() + expiration);
 
-        return Jwts.builder()
+        String token = Jwts.builder()
                 .setIssuer("Stiven's Hotel API")
                 .setSubject(user.getUsername())
                 .setIssuedAt(now)
                 .setExpiration(expirationDate)
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+
+        return new TokenResponseDTO(token);
     }
 
     public String getUsernameFromToken(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
 
-        return claims.getSubject();
+            return claims.getSubject();
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("Token expired");
+        } catch (SignatureException e) {
+            throw new RuntimeException("Invalid token signature");
+        } catch (Exception e) {
+            throw new RuntimeException("Error processing token");
+        }
     }
 
     public boolean isTokenValid(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
