39b7836211a21a89e8a05e49f4732cf9cfc760c4
StivenF0 <stivenfonseca201@gmail.com>
2025-10-14 22:23:41 -0300

    refactor: simplified TokenResponseDTO

--- src/main/java/br/com/stivenshotel/stivens_hotel/dto/auth/TokenResponseDTO.java
+++ src/main/java/br/com/stivenshotel/stivens_hotel/dto/auth/TokenResponseDTO.java
@@ -1,10 +1,5 @@
 package br.com.stivenshotel.stivens_hotel.dto.auth;
 
 public record TokenResponseDTO(
-    String token,
-    String type
-) {
-    public TokenResponseDTO(String token) {
-        this("Bearer", token);
-    }
-}
+    String token
+) {}
