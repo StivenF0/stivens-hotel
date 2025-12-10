4ec421516923103fd66b3bf937ffd2a549fdfc02
A-Weine <weine.sousa082@gmail.com>
2025-12-10 02:17:30 -0300

    backend: ajuste na resposta de login para incluir detalhes de usuario

--- backend/src/main/java/br/com/stivenshotel/stivens_hotel/dto/auth/TokenResponseDTO.java
+++ backend/src/main/java/br/com/stivenshotel/stivens_hotel/dto/auth/TokenResponseDTO.java
@@ -1,5 +1,7 @@
 package br.com.stivenshotel.stivens_hotel.dto.auth;
+import br.com.stivenshotel.stivens_hotel.dto.user.UserResponseDTO;
 
 public record TokenResponseDTO(
-    String token
+    String token,
+    UserResponseDTO user
 ) {}
